0eb4b9004c164dfa188a53365d79cc0c88500a13
Aboood82 <88140315+Aboood82@users.noreply.github.com>
2022-09-06 04:11:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.listen(() => console.log(`ddddddddddddddddddddddddddddddddddddddddddddddd`));
+app.listen(() => console.log(`jus t tsiiiiii stitiititi`));
 
 const Discord = require('discord.js');
 const client = new Discord.Client({
